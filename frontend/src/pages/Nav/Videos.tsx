2b8c8f70e0920f86006579d3fb2c9bae5e498a91
Cecilia <ceci_2504@hotmail.com>
2024-11-07 19:00:51 +0100

    Add styles for videos

--- frontend/src/pages/Nav/Videos.tsx
+++ frontend/src/pages/Nav/Videos.tsx
@@ -35,8 +35,9 @@ const Videos: React.FC = () => {
 
       // Remover duplicados al actualizar edo. Le paso una funcion como estado ((prevVideos) => { ... }); q incluye los valores q ya tiene almacenados
       setVideos((prevVideos) => {
-        const allVideos = [...prevVideos, ...newVideos];//desestructuro esos valores y los nuevos y los almaceno en un nuevo array
-        const uniqueVideos = allVideos.filter(  //se filtra el nuevo array(self) por elemento actual(v) e index para verificar q sea único
+        const allVideos = [...prevVideos, ...newVideos]; //desestructuro esos valores y los nuevos y los almaceno en un nuevo array
+        const uniqueVideos = allVideos.filter(
+          //se filtra el nuevo array(self) por elemento actual(v) e index para verificar q sea único
           (v, index, self) =>
             index === self.findIndex((t) => t.video_id === v.video_id) //compara el video actual (v) con el del array(t) y si está repetido continua avanzando, cuando aparece uno q no coincide(es nuevo) lo agrega a al array uniquevideos
         );
@@ -68,7 +69,7 @@ const Videos: React.FC = () => {
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, []); // 
+  }, []); //
 
   return (
     <div className="videos-wrapper">
@@ -79,6 +80,7 @@ const Videos: React.FC = () => {
         <div key={video.video_id} className="video-item">
           <h2>{video.video_title}</h2>
           <div
+            className="video-content-summary"
             dangerouslySetInnerHTML={{
               __html: video.video_content.slice(0, 500) + "...",
             }}
